d405125e14e8d61f15e21339457269bb9c8f27a3
Raghulskr12 <kalairaghul70@gmail.com>
2026-03-05 07:40:02 +0530

    [ADD] shapefinder with circle and rectangel added

--- Class/ShapeFinder.java
+++ Class/ShapeFinder.java
@@ -23,10 +23,52 @@ class Shape{
     }
 }
 
+
+class Circle extends Shape{
+
+    private double radius;
+
+    Circle(String name,int radius) {
+        super(name);
+        this.radius=radius;
+    }
+
+    @Override
+    public double area() {
+        return 3.14 * radius * radius;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * 3.14 * radius;
+    }
+}
+
+class Rectangle extends Shape
+{
+    private double width;
+    private double height;
+
+    Rectangle(String name,double width,double height ) {
+        super(name);
+        this.height=height;
+        this.width=width;
+    }
+
+    public double area()
+    {
+        return width * height;
+    }
+    public double perimeter()
+    {
+        return 2* (width + height);
+    }
+}
 class ShapeFinder
 {
     public static void main() {
-        Shape s1 = new Shape("Circle");
+        Shape s1 = new Circle("Circle",5);
         s1.describe();
+        System.out.println(s1.area());
     }
 }
